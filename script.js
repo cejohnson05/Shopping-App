f1c8e5ed7deb4f87d38299ac6d97fe66d3f82cb4
cejohnson05 <cejohnson05@gmail.com>
2014-08-27 18:17:13 -0600

    Jquery Updates

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert("script.js");
+$(document).ready(function() {
+	
+	var addItem = ('#addItem');
+	var removeItem = ('#Remove');
+
+		function addNewItem(itemText) {
+			var listItem = document.createElement("li");
+			listItem.innertext = itemText; 
+	}
+	
+	addItem.click(function() {
+		var submitText = document.getElementById("submitText");
+		var itemText = submitText.value;
+	}
+
+	addNewItem(document.getElementById("shoppingList"), itemText);
+
+}); 
